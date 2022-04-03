4f8335732185b3d3a50e1fcea86bf09a60277d73
Sergio <sarb07.au@gmail.com>
2022-04-03 12:39:11 +0930

    Docker's configuration to deploying at development environment. Trusted origins for production added.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,11 @@ then
         sleep 0.1
     done
 
-    echo "PostgresSQL started"
+    echo "PostgreSQL started"
 fi
+
+python manage.py flush --no-input
+python manage.py makemigrations
+python manage.py migrate
 
 exec "$@"
